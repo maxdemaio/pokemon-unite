84f3f31b55192dfb14fda5108c627926e9c0b60c
Maxwell DeMaio <maxdman321@gmail.com>
2021-08-21 10:22:46 -0400

    indentation fix

--- uniteStats.py
+++ uniteStats.py
@@ -8,22 +8,22 @@ import numpy as np, pandas as pd
 with open('data.json','r') as f:
     data = json.loads(f.read())
 
-    # Normalizing data
-    df = pd.json_normalize(data, record_path=["level"], meta=["name"])
+# Normalizing data
+df = pd.json_normalize(data, record_path=["level"], meta=["name"])
 
-    # Create level column
-    df.insert(loc=0, column="lvl",
-        value=np.tile([i for i in range(1,16)], 22))
+# Create level column
+df.insert(loc=0, column="lvl",
+    value=np.tile([i for i in range(1,16)], 22))
 
-    print(df)
+print(df)
 
-    # Group data by level
-    grouped = df.groupby(["lvl"])
-    
-    # Analyze by level
-    for i in range(1, 16):
-        grouped.get_group(i).to_csv(
-            "uniteCSVs/uniteLevel{}.csv".format(i), encoding='utf-8', index=False)
+# Group data by level
+grouped = df.groupby(["lvl"])
+
+# Analyze by level
+for i in range(1, 16):
+    grouped.get_group(i).to_csv(
+        "uniteCSVs/uniteLevel{}.csv".format(i), encoding='utf-8', index=False)
 
 
 
